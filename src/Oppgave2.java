e29571a71e27f483e0be971e349ed2360d1ebd58
Mr-Smitt <jacob@paal.org>
2021-10-05 18:44:47 +0200

    Andre prototype av Oppgave 2. Hvor jeg fjernet en unødvendig else

--- src/Oppgave2.java
+++ src/Oppgave2.java
@@ -7,9 +7,7 @@ public class Oppgave2 {
         }
         int n = 1;
         for (int i=0; i+1 < a.length; i++) {
-            if (a[i] == a[i+1]) {
-            }
-            else {
+            if (a[i] != a[i+1]) {
                 n++;
             }
         }
