00eff9ce4d30ec3c10123e6445c033c9490d96c8
Mr-Smitt <jacob@paal.org>
2021-10-05 08:29:08 +0200

    Første prototype av maks metoden før jeg begynner å lage ombyttinger

--- src/Oppgave1.java
+++ src/Oppgave1.java
@@ -1,6 +1,22 @@
+
 public class Oppgave1 {
     public static int maks(int[]a) {
+        for (int i = 0; i+1 < a.length;) {
+            System.out.println(i);
+            if (a[i] < a[i+1]) {
+                a[i] = a[i+1];
+                i = 0;
+            } else {
+                i = i + 1;
+            }
+            System.out.println(i);
+        }
+        return a[0];
+    }
+    public static void main(String[] args) {
 
-
+        int[] a = {1, 2, 3, 4, 5};
+        int b=maks(a);
+        System.out.println(b);
     }
 }
