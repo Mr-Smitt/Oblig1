107df31cb0b10c09dcac7c83dcc80d09312094f9
Mr-Smitt <jacob@paal.org>
2021-10-05 18:46:12 +0200

    Siste commit uten main metode og out.println

--- src/Oppgave2.java
+++ src/Oppgave2.java
@@ -13,10 +13,4 @@ public class Oppgave2 {
         }
         return n;
     }
-
-    public static void main(String[] args) {
-        int[] a={1,1,2,2,4,4,5,5,6,6,7,7,8};
-        int b =antallUlikeSortert(a);
-        System.out.println(b);
-    }
 }
